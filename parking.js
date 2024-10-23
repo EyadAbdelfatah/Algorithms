2264fe9ceac76b53895276ae7e1179515742b0b5
mellowed24 <eyada5@nycstudents.net>
2024-10-22 21:01:18 -0400

    jkn

--- parking.js
+++ parking.js
@@ -1,12 +1,12 @@
-function parking(number,yesterday,today){
+function parking(number, yesterday, today) {
     let result = 0;
-    for(let i = 1;i<=number;i++){
-        if (yesterday[i] && today[i] === "C"){
+    for (let i = 0; i < number; i++) {
+        if (yesterday[i] === "C" && today[i] === "C") {
             result++;
         }
     }
-    return result
+    return result;
 }
-y=["C","C","C"]
-z=["C","C","."]
-console.log(parking(3,y,z))
+y = ["C", "C", "C"]
+z = ["C", "C", "."]
+console.log(parking(3, y, z));
