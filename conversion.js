f666c8cedc3286099085658e1788e5bbd3c9f6d1
mellowed24 <eyada5@nycstudents.net>
2024-10-24 11:01:59 -0400

    sdkub

--- conversion.js
+++ conversion.js
@@ -1,20 +1,25 @@
-function binaryConversion(number){
-    let result = "";
-    let remainder;
-    while(number>=1){
-        remainder = number % 2;
-        number= Math.floor(number/2)
-        if (remainder === 1){
-            result+="1"
-
-        }
-        else{
-            result+="0"
-        }
-
-
+function ToBinaryConversion(number) {
+  let result = "";
+  let remainder;
+  while (number >= 1) {
+    remainder = number % 2;
+    number = Math.floor(number / 2);
+    if (remainder === 1) {
+      result += "1";
+    } else {
+      result += "0";
     }
-    result =  result.split('').reverse().join('');
-    return result;
+  }
+  result = result.split("").reverse().join("");
+  return result;
 }
-console.log(binaryConversion(73))
+
+function toNumberConversion(binary) {
+  let result = 0;
+  binary = binary.toString().split("").reverse("").map(Number);
+  for (let i = 1; i <= binary.length; i++) {
+    result += binary[i] * Math.pow(2, i);
+  }
+  return result;
+}
+console.log(toNumberConversion(1011));
